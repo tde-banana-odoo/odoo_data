c94b5964271ae3c0d26275cab661202f17b39aa2
Thibault Delavallée <tde@odoo.com>
2016-12-07 13:52:44 +0100

    [ADD] helpdesk_data: basic data for helpdesk

---
+++ helpdesk_data/__manifest__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+{
+    'name': 'Sale Data',
+    'version': '1.0',
+    'category': 'Tools',
+    'complexity': 'easy',
+    'description': """
+This module adds vigorous data. For my tests.
+==============================================
+    """,
+    'depends': ['base_data', 'helpdesk'],
+    'data': [
+        'data/res_partner_data.xml',
+        'data/helpdesk_data.xml',
+    ],
+    'installable': True,
+    'auto_install': True,
+}
