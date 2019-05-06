43c075166ea9bb8f3bc83110ada22995de73d892
Thibault Delavallée <tde@odoo.com>
2019-05-06 15:07:10 +0200

    [IMP] websites_slide_data: update data to eLearning

--- website_slides_data/__manifest__.py
+++ website_slides_data/__manifest__.py
@@ -13,8 +13,10 @@ This module adds vigorous data. For my tests.
     'depends': ['base_data', 'website_slides'],
     'data': [
         'data/res_partner_data.xml',
+        'data/slide_channel_tag_data.xml',
         'data/slide_channel_data.xml',
         'data/slide_slide_data.xml',
+        'data/slide_user_data.xml',
     ],
     'installable': True,
     'auto_install': True,
