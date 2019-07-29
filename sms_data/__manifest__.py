467fdfcf0516d74a953467ffd1ccec04ac9cab52
Thibault Delavallée <tde@odoo.com>
2019-07-29 10:38:40 +0200

    [ADD] sms_data: data for sms module
    Containing here only dummy configuration for IAP. More content will come
    in 13+ versions.

---
+++ sms_data/__manifest__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+{
+    'name': 'SMS Data',
+    'version': '1.0',
+    'category': 'Tools',
+    'complexity': 'easy',
+    'description': """
+This module adds vigorous data. For my tests.
+==============================================
+    """,
+    'depends': ['base_data', 'mail_data', 'sms'],
+    'data': [
+        'data/configuration_data.xml',
+        'data/configuration_data_private.xml',  # comment if necessary
+        'data/configuration_data_private_11.xml',  # comment if necessary
+        # 'data/configuration_data_private_12.xml',  # comment if necessary
+    ],
+    'installable': True,
+    'auto_install': True,
+}
